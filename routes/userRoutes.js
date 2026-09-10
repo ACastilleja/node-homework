755008c7a41e7d6e4c69570baa96b7778912cc65
ACastilleja <castillejaarturo@gmail.com>
2026-09-10 19:04:26 -0400

    added googleAuthController and edited userRoutes to include googleLogon

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,10 +2,12 @@ const express = require("express");
 const router = express.Router();
 const jwtMiddleware = require("../middleware/jwtMiddleware");
 
-const {register, logon, logoff }= require("../controllers/userController");
+const {register, logon, logoff, googleLogon }= require("../controllers/userController");
+
 
 router.post("/register", register);
 router.post("/logon", logon);
 router.post("/logoff", jwtMiddleware, logoff);
+router.post("/googleLogon", googleLogon);
 
 module.exports = router;
